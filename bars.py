e3dab0afcb558f532e681971642ad9057b343c44
IvanGamazov <ivan.gamazov@gmail.com>
2016-09-29 16:16:18 +0300

    С клавиатуры вводится полное имя файла с данными . например /Volumes/Disk1/data.json
    Далее необходимо ввести долготу и широту по соответствующему запросу
    программы.

--- bars.py
+++ bars.py
@@ -1,21 +1,44 @@
 import json
+from pprint import pprint
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as data_file:
+        data = json.load(data_file)
+    return data
 
 
 def get_biggest_bar(data):
-    pass
+    maxseats = max(data, key=lambda d: d['Cells']['SeatsCount'])
+    for x in data:
+        if x['Cells']['SeatsCount'] == maxseats['Cells']['SeatsCount']:
+            pprint(x)
 
 
 def get_smallest_bar(data):
-    pass
+    minseats = min(data, key=lambda d: d['Cells']['SeatsCount'])
+    for x in data:
+        if x['Cells']['SeatsCount'] == minseats['Cells']['SeatsCount']:
+            pprint(x)
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    nearest_bar = min(data, key=lambda x:((x['Cells']['geoData']['coordinates'][0] - longitude)**2 + (x['Cells']['geoData']['coordinates'][1] - latitude)**2))
+    pprint(nearest_bar)
+
+
+def main():
+    filepath = input("Enter data file name --> ")
+    data = load_data(filepath)
+    pprint('--------------------Smallest Bars---------------------')
+    get_smallest_bar(data)
+    pprint('--------------------Biggest Bars---------------------')
+    get_biggest_bar(data)
+    pprint('--------------------Closest Bar---------------------')
+    longitude = input("Enter your longitude --> ")
+    latitude = input("Enter your latitude --> ")
+    get_closest_bar(data, float(longitude), float(latitude))
 
 
 if __name__ == '__main__':
-    pass
+    main()
